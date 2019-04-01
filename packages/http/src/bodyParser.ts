021d6dbccec684fe02f6abab5b82bd9b2b11d82a
Krzysztof Miemiec <krzysztof.miemiec@gmail.com>
2019-04-01 11:04:04 +0200

    * Improved keywords and small asset cleanup * Body parser content-type detection fix

--- packages/http/src/bodyParser.ts
+++ packages/http/src/bodyParser.ts
@@ -35,26 +35,32 @@ export const bodyParser = ({
 }: BodyParserOptions) => {
   const caseConverter = getCaseConverter(bodyCasing);
   return (response: HttpResponse): HttpResponse => {
-    if (response.parsedBody) {
-      return response;
-    }
-    switch (response.headers.get('content-type')) {
-      case ContentType.JSON:
-      case ContentType.VND_JSON:
-        response.parsedBody = () => response.json()
-          .then(caseConverter);
-        break;
-      case ContentType.URL_ENCODED:
-        response.parsedBody = () => response.text()
-          .then(parseUrlEncodedBody)
-          .then(caseConverter);
-        break;
-      case ContentType.TEXT:
-        response.parsedBody = () => response.text();
-        break;
-      default:
-        response.parsedBody = () => response.arrayBuffer();
-        break;
+    const contentType = (response.headers.get('content-type') || '')
+      .split(';')
+      .map(type => type.trim().toLowerCase());
+
+    for (const type of contentType) {
+      if (response.parsedBody) {
+        return response;
+      }
+      switch (type) {
+        case ContentType.JSON:
+        case ContentType.VND_JSON:
+          response.parsedBody = () => response.json()
+            .then(caseConverter);
+          break;
+        case ContentType.URL_ENCODED:
+          response.parsedBody = () => response.text()
+            .then(parseUrlEncodedBody)
+            .then(caseConverter);
+          break;
+        case ContentType.TEXT:
+          response.parsedBody = () => response.text();
+          break;
+        default:
+          response.parsedBody = () => response.arrayBuffer();
+          break;
+      }
     }
     return response;
   };
