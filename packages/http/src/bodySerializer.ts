b808a217082c5cca048083cb86d2cd718f0b39db
Krzysztof Miemiec <krzysztof.miemiec@gmail.com>
2020-02-11 20:21:40 -0800

    refactor(body-parser): new content-type detection
    * improved content-type detection - switchContentType utility
    * both body parser & serializer use new mechanism

--- packages/http/src/bodySerializer.ts
+++ packages/http/src/bodySerializer.ts
@@ -1,9 +1,10 @@
 import isNil from 'lodash/isNil';
 import isString from 'lodash/isString';
 import isBuffer from 'lodash/isBuffer';
-import { BodySerializer, ContentType, HttpOptions, NormalizedHttpBody } from './httpClient.types';
+import { BodySerializer, HttpOptions, NormalizedHttpBody } from './httpClient.types';
 import { BodyCasing, getCaseConverter, getHeader } from './helpers';
 import { urlEncode } from './helpers/urlEncoding.helper';
+import { ContentTypeMap, switchContentType } from './contentType';
 
 export interface BodySerializerOptions {
   bodyCasing?: BodyCasing;
@@ -13,6 +14,13 @@ export const bodySerializer = ({
   bodyCasing,
 }: BodySerializerOptions = {}): BodySerializer => {
   const caseConverter = getCaseConverter(bodyCasing);
+
+  const bodySerializers: ContentTypeMap<(body: any) => any> = {
+    JSON: (body) => JSON.stringify(caseConverter(body)),
+    URL_ENCODED: (body) => urlEncode(caseConverter(body)),
+    TEXT: (body) => String(body),
+  };
+
   return (options: HttpOptions): NormalizedHttpBody => {
     const contentTypeHeader = getHeader(options.headers, 'content-type') || '';
     const contentType = contentTypeHeader.split(';')
@@ -24,16 +32,9 @@ export const bodySerializer = ({
     }
     if (typeof body === 'object') {
       for (const type of contentType) {
-        switch (type) {
-          case ContentType.JSON:
-          case ContentType.VND_JSON:
-            return JSON.stringify(caseConverter(options.body));
-          case ContentType.URL_ENCODED:
-            return urlEncode(caseConverter(options.body));
-          case ContentType.TEXT:
-            return String(body);
-          default:
-            break;
+        const serializedBody = switchContentType(type, bodySerializers);
+        if (serializedBody) {
+          return serializedBody(body);
         }
       }
     }
