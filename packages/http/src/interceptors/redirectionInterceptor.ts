cf392d7d1acc599ba2fb1d1e89d1ef2cef3661d0
Krzysztof Miemiec <krzysztof.miemiec@gmail.com>
2020-02-11 20:24:20 -0800

    feat(redirections): redirection support
    previously coolio supported redirections when request handler had such option built-in. now it's a feature extracted to an interceptor

---
+++ packages/http/src/interceptors/redirectionInterceptor.ts
@@ -0,0 +1,29 @@
+import { HttpInterceptorFunction } from '../httpClient.types';
+import { HttpResponseError } from '../httpResponseError';
+
+export interface RedirectionInterceptorOptions {
+  redirectionLimit?: number;
+}
+
+export const createRedirectionInterceptor = ({
+  redirectionLimit = 30
+}: RedirectionInterceptorOptions): HttpInterceptorFunction => (request, options) => async () => {
+  let response = await request();
+  let redirectionCount = 0;
+  while (response.status >= 300 && response.status < 400) {
+    if (++redirectionCount > redirectionLimit) {
+      throw new HttpResponseError(
+        response,
+        `Redirection limit (of ${redirectionLimit} redirections) has been exceeded`,
+      );
+    }
+    const location = response.headers.get('location');
+    if (location) {
+      options.url = location;
+      response = await request();
+    } else {
+      break;
+    }
+  }
+  return response;
+};
