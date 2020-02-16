269b0f59ef125fe317dba6d2660f6264c51c0a19
Krzysztof Miemiec <krzysztof.miemiec@gmail.com>
2020-02-16 23:43:08 +0100

    test(encoding): added tests for converting text to arraybuffer

--- packages/http/src/helpers/encoder.helper.ts
+++ packages/http/src/helpers/encoder.helper.ts
@@ -2,11 +2,25 @@ import TypedArray = NodeJS.TypedArray;
 import { HttpResponseHeaders } from '../httpResponseHeaders';
 import { Buffer } from 'buffer';
 
-/* TextEncoder is not available on every environment, hence in this utility
+export type BufferEncoding =
+  | 'ascii'
+  | 'utf8'
+  | 'utf-8'
+  | 'utf16le'
+  | 'ucs2'
+  | 'ucs-2'
+  | 'base64'
+  | 'latin1'
+  | 'binary'
+  | 'hex'
+  ;
+
+/*
+ * TextEncoder is not available on every environment, hence in this utility
  * we use "buffer" module (the npm one, not Node.js).
  */
 
-export const getEncodingFromHeaders = (headers: HttpResponseHeaders | undefined, fallback = 'utf-8') => (
+export const getEncodingFromHeaders = (headers: HttpResponseHeaders | undefined, fallback = 'utf8') => (
   headers?.get('content-type')
     ?.split(';')
     ?.find(element => element.trim().startsWith('charset'))
@@ -14,11 +28,11 @@ export const getEncodingFromHeaders = (headers: HttpResponseHeaders | undefined,
   ?? fallback
 ).trim().toLowerCase();
 
-export const encodeArrayBuffer = (data: TypedArray | string | undefined = '') =>
+export const encodeArrayBuffer = (data: TypedArray | string | undefined = '', encoding: BufferEncoding = 'utf8') =>
   typeof data === 'string'
-    ? new Uint8Array(Buffer.from(data, 'utf8'))
+    ? new Uint8Array(Buffer.from(data, encoding as BufferEncoding))
     : new Uint8Array(Buffer.from(data));
 
-export const encodeText = (buffer: ArrayBuffer, charset = 'utf-8') => {
-  return Buffer.from(buffer).toString(charset);
+export const encodeText = (buffer: ArrayBuffer, encoding = 'utf8') => {
+  return Buffer.from(buffer).toString(encoding);
 };
